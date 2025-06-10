5ab12b3c92857674fe1e8bccd978da247c62842b
hrncacz <hrncacz@gmail.com>
2025-06-10 20:24:32 +0200

    kompletní text_to_text_node

--- src/components.py
+++ src/components.py
@@ -20,11 +20,35 @@ def text_node_to_html_node(text_node):
         case _:
             raise Exception("Invalid TEXT TYPE")
 
+
+def delimiter_from_text_type(text_type):
+    match text_type:
+        case TextType.TEXT:
+            return " "
+        case TextType.ITALIC:
+            return "_"
+        case TextType.BOLD:
+            return "**"
+        case TextType.CODE:
+            return "`"
+        case TextType.LINK:
+            return ("[", "](", ")")
+        case TextType.IMAGE:
+            return ("![", "](", ")")
+        case _:
+            raise Exception("Invalid TEXT TYPE")
+
 # "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
 
 
 def split_nodes_link_and_image(old_nodes, text_type):
     new_nodes = []
+    checking_node = old_nodes[0]
+    if checking_node.text_type != TextType.TEXT:
+        new_nodes.append(checking_node)
+    else:
+        new_nodes.extend(check_text_for_link_and_image(
+            checking_node.text, text_type))
 
     if len(old_nodes) == 1:
         return new_nodes
@@ -33,24 +57,25 @@ def split_nodes_link_and_image(old_nodes, text_type):
     return new_nodes
 
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
+def split_nodes_delimiter(old_nodes, text_type):
     new_nodes = []
     checking_node = old_nodes[0]
     if checking_node.text_type != TextType.TEXT:
         new_nodes.append(checking_node)
     else:
         new_nodes.extend(check_text_for_delimiter(
-            checking_node.text, delimiter, TextType.TEXT, text_type))
+            checking_node.text,  TextType.TEXT, text_type))
 
     if len(old_nodes) == 1:
         return new_nodes
     new_nodes.extend(split_nodes_delimiter(
-        old_nodes[1:], delimiter, text_type))
+        old_nodes[1:], text_type))
     return new_nodes
 
 
-def check_text_for_delimiter(sentence, delimiter, finding_text_type, text_type):
+def check_text_for_delimiter(sentence, finding_text_type, text_type):
     nodes = []
+    delimiter = delimiter_from_text_type(text_type)
     if finding_text_type != TextType.TEXT:
         check = sentence.find(delimiter)
         if check == -1:
@@ -62,18 +87,56 @@ def check_text_for_delimiter(sentence, delimiter, finding_text_type, text_type):
     if len(splited_text) == 1:
         return nodes
     if finding_text_type != text_type:
-        return nodes + check_text_for_delimiter(splited_text[1], delimiter, text_type, text_type)
+        return nodes + check_text_for_delimiter(splited_text[1],  text_type, text_type)
     else:
-        return nodes + check_text_for_delimiter(splited_text[1], delimiter, TextType.TEXT, text_type)
+        return nodes + check_text_for_delimiter(splited_text[1],  TextType.TEXT, text_type)
 
 
-def check_text_for_linl_and_image(sentence, delimiter, text_typw):
+def check_text_for_link_and_image(sentence, text_type):
+    if text_type != TextType.LINK and text_type != TextType.IMAGE:
+        raise Exception("Wrong TextType passed")
     nodes = []
-    splited_text = sentence.split(delimiter)
+    splited_text = sentence.split(delimiter_from_text_type(text_type)[0], 1)
+    if len(splited_text[0]) != 0:
+        nodes.append(TextNode(splited_text[0], TextType.TEXT))
+    if len(splited_text) == 1:
+        return nodes
+    try:
+        link_image_tuple, rest_of_sentence = find_alt_text(
+            splited_text[1], text_type)
+    except Exception as e:
+        raise e
+    nodes.append(
+        TextNode(link_image_tuple[0], text_type, link_image_tuple[1]))
+    return nodes + check_text_for_link_and_image(rest_of_sentence, text_type)
 
-    pass
+
+def find_alt_text(sentence, text_type):
+    splited_text = sentence.split(delimiter_from_text_type(text_type)[1], 1)
+    if len(splited_text) == 1:
+        raise Exception(
+            f"Missing closing brackets of LINK/IMAGE node in --- {sentence}")
+    try:
+        url, rest_of_sentence = find_url(splited_text[1], text_type)
+    except Exception as e:
+        raise e
+    alt_text_url_tuple = (splited_text[0], url)
+    return alt_text_url_tuple, rest_of_sentence
 
 
-def find_alt_text(sentence):
-    arr = []
-    pass
+def find_url(sentence, text_type):
+    splited_text = sentence.split(delimiter_from_text_type(text_type)[2], 1)
+    if len(splited_text) == 1:
+        raise Exception(
+            f"Missing closing brackets of of LINK/IMAGE node in --- {sentence}")
+    return splited_text[0], splited_text[1]
+
+
+def text_to_text_nodes(text):
+    text_codes = split_nodes_delimiter(
+        [TextNode(text, TextType.TEXT)], TextType.CODE)
+    text_bolds = split_nodes_delimiter(text_codes, TextType.BOLD)
+    text_italics = split_nodes_delimiter(text_bolds, TextType.ITALIC)
+    text_images = split_nodes_link_and_image(text_italics, TextType.IMAGE)
+    text_links = split_nodes_link_and_image(text_images, TextType.LINK)
+    return text_links
--- src/test_components.py
+++ src/test_components.py
@@ -2,7 +2,7 @@ import unittest
 
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from components import text_node_to_html_node, split_nodes_delimiter, split_nodes_link_and_image
+from components import text_node_to_html_node, split_nodes_delimiter, split_nodes_link_and_image, text_to_text_nodes
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -26,30 +26,54 @@ class TestHTMLNode(unittest.TestCase):
     def test_split_nodes_delimiter(self):
         node1 = TextNode(
             "This is text with a `code block` word", TextType.TEXT)
-        new_nodes1 = split_nodes_delimiter([node1], "`", TextType.CODE)
+        new_nodes1 = split_nodes_delimiter([node1],  TextType.CODE)
         self.assertEqual(new_nodes1, [TextNode("This is text with a ", "normal", None), TextNode(
             "code block", "code", None), TextNode(" word", "normal", None)])
 
         node2 = TextNode(
             "This is text with a **bold block** and also _italic block_ word", TextType.TEXT)
-        bold_nodes2 = split_nodes_delimiter([node2], "**", TextType.BOLD)
+        bold_nodes2 = split_nodes_delimiter([node2],  TextType.BOLD)
         italic_nodes2 = split_nodes_delimiter(
-            bold_nodes2, "_", TextType.ITALIC)
+            bold_nodes2,  TextType.ITALIC)
         self.assertEqual(italic_nodes2, [TextNode("This is text with a ", "normal", None), TextNode("bold block", "bold", None), TextNode(
             " and also ", "normal", None), TextNode("italic block", "italic", None), TextNode(" word", "normal", None)])
         node3 = TextNode(
             "This is text with a **bold block** and also _italic block word", TextType.TEXT)
-        bold_nodes3 = split_nodes_delimiter([node3], "**", TextType.BOLD)
+        bold_nodes3 = split_nodes_delimiter([node3],  TextType.BOLD)
         with self.assertRaises(Exception):
             italic_nodes3 = split_nodes_delimiter(
-                bold_nodes3, "_", TextType.ITALIC)
+                bold_nodes3,  TextType.ITALIC)
 
     def test_split_nodes_link_and_image(self):
 
-        text_node = TextNode(
+        text_node1 = TextNode(
             "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
-        node = split_nodes_link_and_image([text_node], TextType.LINK)
-        print("!Test text pro split,".split("!", 1))
+        node1 = split_nodes_link_and_image([text_node1], TextType.LINK)
+        self.assertEqual(node1, [TextNode("This is text with a link ", TextType.TEXT, None), TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                         TextNode(" and ", TextType.TEXT, None), TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev")])
+
+        text_node2 = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
+        text_node2_2 = TextNode(
+            "This is text with a link ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
+        text_node2_3 = TextNode(
+            "This is text with a link ![to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
+        node2 = split_nodes_link_and_image(split_nodes_link_and_image(
+            [text_node2, text_node2_2, text_node2_3], TextType.IMAGE), TextType.LINK)
+        self.assertEqual(node2, [TextNode("This is text with a link ", TextType.TEXT, None), TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"), TextNode(" and ", TextType.TEXT, None), TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"), TextNode("This is text with a link ", TextType.TEXT, None), TextNode("to boot dev", TextType.IMAGE, "https://www.boot.dev"),
+                         TextNode(" and ", TextType.TEXT, None), TextNode("to youtube", TextType.IMAGE, "https://www.youtube.com/@bootdotdev"), TextNode("This is text with a link ", TextType.TEXT, None), TextNode("to boot dev", TextType.IMAGE, "https://www.boot.dev"), TextNode(" and ", TextType.TEXT, None), TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev")])
+
+        text_node3 = TextNode(
+            "![to boot dev](https://www.boot.dev)[to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
+        node3 = split_nodes_link_and_image(split_nodes_link_and_image(
+            [text_node3], TextType.IMAGE), TextType.LINK)
+        self.assertEqual(node3, [TextNode("to boot dev", TextType.IMAGE, "https://www.boot.dev"),
+                         TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev")])
+
+    def test_text_to_text_nodes(self):
+        text1 = "Tohle je testovací **bold text** a toto zase _italic text_ v tomhle bloku otestuju `code text` teď třeba image ![seznam cz](https://www.seznam.cz) a poslední je link [google com](https://www.google.com) a konec."
+        self.assertEqual(text_to_text_nodes(text1), [TextNode("Tohle je testovací ", TextType.TEXT, None), TextNode("bold text", TextType.BOLD, None), TextNode(" a toto zase ", TextType.TEXT, None), TextNode("italic text", TextType.ITALIC, None), TextNode(" v tomhle bloku otestuju ", TextType.TEXT, None), TextNode(
+            "code text", TextType.CODE, None), TextNode(" teď třeba image ", TextType.TEXT, None), TextNode("seznam cz", TextType.IMAGE, "https://www.seznam.cz"), TextNode(" a poslední je link ", TextType.TEXT, None), TextNode("google com", TextType.LINK, "https://www.google.com"), TextNode(" a konec.", TextType.TEXT, None)])
 
 
 if __name__ == "__main__":
